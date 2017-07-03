c6ce185b9de7c9ca9465ef1f7ae9e49e408f1d39
yekki <gary.niu@gmail.com>
2017-07-03 16:56:23 +0800

    init

--- today/views.py
+++ today/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render, HttpResponse
+import os
 
+albums = {'lyh':'罗永浩干货日记', 'wj':'硅谷来信', 'xy':'熊逸书院', 'yss':'西方艺术史'}
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    root_dir = '/Users/gniu/Temp.localized/get'
+    album_list = list()
+
+    album_dirs = os.listdir(root_dir)
+
+    for d in album_dirs:
+        dir = os.path.join(root_dir, d)
+        if os.path.isdir(dir):
+            album_list.append(albums[d])
+
+    return render(request, 'today/index.html', {'albums':album_list})
+
+
+def album(request):
+    pass
